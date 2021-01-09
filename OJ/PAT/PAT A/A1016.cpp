f780bdbed7111729631bf0841b6e2cc04fba5d54
Ben <hor4bynZ@gmail.com>
2021-01-09 18:06:18 +0800

    A1016.cpp(uncompleted twice :-( )

--- OJ/PAT/PAT A/A1016.cpp
+++ OJ/PAT/PAT A/A1016.cpp
@@ -13,6 +13,8 @@ struct man_t {
 	bool lab;//label
 }man[1001];
 
+int cen[24] = { 0 };//cent
+
 bool cmp(struct man_t a, struct man_t b) {
 	if (a.nam != b.nam)    return strcmp(a.nam, b.nam) < 0;
 	else if (a.dd != b.dd)    return a.dd < b.dd;
@@ -20,8 +22,61 @@ bool cmp(struct man_t a, struct man_t b) {
 	else    return a.mm < b.mm;
 }
 
-int calc_time(struct man_t a, struct man_t b) {
-	return 1;
+void calc_time(struct man_t a, struct man_t b, int *dol, int *dur) {//dollar, duration
+	/*
+	 * 1
+	 * int t0 = 0, n = 23
+	 * while(T1>t0) t0++;
+	 * while(T2<tn) tn--;
+	 *
+	 * 2
+	 * min = t0-T1
+	 * dol += cen[t0-1]*min
+	 * dur += min
+	 *
+	 * min = T2-tn
+	 * dol += cen[tn]*min
+	 * dur += min
+	 *
+	 * 3
+	 * for(){
+	 *  d = dd1:HH1 - dd2:HH2
+	 *  if(d) 转化为hour
+	 * }
+	 *
+	 * 4
+	 * for(i;次数;++i, ++t0){
+	 *  if(t0 == 24) t0 = 0;
+	 *  dol += cen[t0]*60
+	 *  dur += 60
+	 * }
+	 */
+	int T1 = a.HH, T2 = b.HH;
+	int t0 = 0, tn = 23;
+	while (T1 > t0)    t0++;
+	while (T2 < tn)    tn--;
+
+	int min;
+	min = t0 - T1;
+	dol = dol + cen[t0 - 1] * min;
+	dur += min;
+
+	min = T2 - tn;
+	dol = dol + cen[tn] * min;
+	dur += min;
+
+	int dif = 0;
+	if (a.HH < b.HH) {
+		dif = a.HH + 24 - b.HH;
+		a.dd--;
+	} else    dif = a.HH - b.HH;
+	dif = dif + (a.dd - b.dd) * 24;
+
+	for (int i = 0; i < dif; ++i, ++t0) {
+		if (t0 == 24)    t0 = 0;
+		dol = dol + cen[t0] * 60;
+		dur += 60;
+	}
 }
 
 int main() {
