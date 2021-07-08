c46f36f9e43f392c717348c9e1486b27e2420f15
hoRbyn <hor4bynZ@gmail.com>
2021-07-08 13:53:35 +0800

    A1043 PA, 14/25; A1086 PA 21/25

--- OJ/PAT/PAT A/A1043.cpp
+++ OJ/PAT/PAT A/A1043.cpp
@@ -1,80 +1,110 @@
+/* PA 14/25, 看扣分相差 10+ 分, 基本就是思路错了 */
+/*
+ * 我以为题目给出的是 BST 的前序遍历序列让我判断是不是正序/反序 BST
+ *     所以以下思路是"知前序序列判断是否 BST":
+ *         因为是前序序列, 序列第一个元素必定是根。则对于正序(逆序) BST,
+ *     其左子树均 ＜(≥) 根而右子树均 ≥(＜) 根, 所以序列中第一个出现 ≥(＜)
+ *     根的结点便是右子树。利用这一特征可以对序列递归地进行左右子树的划分进
+ *     而建树
+ * 而题目实际的思路是给出的序列只是输入序列, 也就是顺序给出结点让你按 BST
+ *     特性进行插入, 进入建树。那么思路就完全不同了(我感觉题目应该给得更明
+ * 确一些, 比如应该将"Now given a sequence of integer keys, you are 
+ * supposed to tell if..", 改为"Now given a sequence of INSERTION of
+ * integer keys..")
+ */
 #include <iostream>
 #include <vector>
 using namespace std;
 
-/*
-暂时发现了一个逻辑错误的例子：
-5
-2 4 3 5 1
-无论是 BST 还是 BST 镜像最后输出应该是 "NO" 而目前程序输出 "YES"
-
-错误原因是：
-（如果按 BST 来组织）既然 [1] 比 [0] 大，那么 [1] 后面的结点应该全是右子树————全比 [0] 大；（如果按 BST 镜像来组织）既然 [1] 比 [0] 大，那么 [1]~[3] 应该是左子树————但这棵左子树又不满足 BST 特性
-
-修改了很久都没改出来，所以先放着 :-(
-*/
-
-struct node_t {
+typedef struct node_t {
 	int data;
-	node_t* left;
-	node_t* right;
-};
+	struct node_t* left, * right;
+}node_t;
 
-static const int maxn = 1000;
-static int tree[maxn];
-static bool flag = true;
-static vector<int> post_order;//由于打印有格式，所以先统一放入数组
+const int maxn = 1000;
+int n;
+int a[maxn];
+vector<int > post;
 
-static node_t* pre_order_create(int base, int end) {
+/* 判断是否正序或反序的 BST: 若是正序则返回 1, 反序则返回 0, 若不是 BST 则返回 -1 */
+int judge_bst_seq() {
+	int k = 0;
+	if (a[1] < a[0]) {//第二个元素比第一个少则为正序 BST
+		for (int i = 2; i < n; ++i) {
+			if (a[i] >= a[0]) {//遇到第一个 ≥ 根结点即右子树
+				k = i;
+				break;
+			}
+		}
+		for (; k < n; ++k) {//检查右子树是否满足所有结点均 ≥ 根结点
+			if (a[k] < a[0])    break;
+		}
+		if (k == n)    return 1;//若是 BST 则能遍历到底
+		else    return -1;			//否则中途退出必定不是 BST
+	} else {//第二个元素比第一个少则为逆序 BST
+		for (int i = 2; i < n; ++i) {
+			if (a[i] < a[0]) {//遇到第一个 ＜ 根结点即右子树
+				k = i;
+				break;
+			}
+		}
+		for (; k < n; ++k) {//检查右子树是否满足所有结点均 ＜ 根结点
+			if (a[k] >= a[0])    break;
+		}
+		if (k == n)    return 0;//若是 BST 则能遍历到底
+		else    return -1;			//否则中途退出必定不是 BST
+	}
+}
+
+/* 后序遍历 */
+void post_travesal(node_t *t) {
+	if (t == NULL)    return;
+
+	post_travesal(t->left);
+	post_travesal(t->right);
+	post.push_back(t->data);
+}
+
+/* 根据正序或逆序 BST 创建树: 其中数组范围是闭区间即 [h, t] */
+node_t* create(int h, int t, bool f) {
 	node_t* node = new node_t;
-	node->data = tree[base];
+	node->data = a[h];
 
-	if (base == end) {
+	if (h == t) {
 		node->left = node->right = NULL;
 		return node;
 	}
 
-	int l = base + 1, r = end;
-	if (tree[base + 1] < node->data) {//左子树比根小
-		for (; l <= end && tree[l] < node->data; ++l);//l 遍历结束为第一个右子树结点
-		for (; r > base && tree[r] >= node->data; --r);//r 遍历结束为最后一个左子树结点
-	} else {
-		for (; l <= end && tree[l] >= node->data; ++l);
-		for (; r > base && tree[r] < node->data; --r);
+	int k = h;
+	for (int i = h + 1; i <= t; ++i) {
+		if ((f == true && a[i] >= a[k])
+			|| (f == false && a[i] < a[k])) {//正序且当前结点 ≥ 根, 或逆序且当前结点＜根
+			k = i;//k 是左右子树分界
+			break;
+		}
 	}
-	if (l - 1 != r)    flag = false;
-
-	node->left = pre_order_create(base + 1, l - 1);
-	if (l <= end)    node->right = pre_order_create(r + 1, end);
-	else    node->right = NULL;
+	node->left = create(h + 1, k - 1, f);
+	node->right = create(k, t, f);
 	return node;
 }
 
-static void post_order_traversal(node_t* t) {
-	if (t == NULL)    return;
-
-	post_order_traversal(t->left);
-	post_order_traversal(t->right);
-	post_order.push_back(t->data);
-}
-
 int main() {
-	int n;
-
-	/* 1. 输入 */
+	/* 1. INPUT MODULE */
 	cin >> n;
-	for (int i = 0; i < n; ++i)    cin >> tree[i];
+	for (int i = 0; i < n; ++i)    cin >> a[i];
 
-	/* 2. 主逻辑 */
-	node_t* tree = pre_order_create(0, n - 1);
+	/* 2. MAIN LOGIC */
+	int flag = judge_bst_seq();
+	if (flag == -1) {
+		cout << "NO";
+		return 0;
+	}
+	node_t* tree = create(0, n - 1, (bool)flag);
+	post_travesal(tree);
 
-	/* 3. 输出 */
-	if (flag) {
-		post_order_traversal(tree);
-		cout << "YES\n" << post_order[0];
-		for (unsigned i = 1; i < post_order.size(); ++i)
-			cout << " " << post_order[i];
-	} else    cout << "NO";
+	/* 3. OUTPUT MODULE */
+	cout << "YES\n" << post[0];
+	for (int i = 1; i < (int)post.size(); ++i)    cout << " " << post[i];
 
 	return 0;
 }
--- OJ/PAT/PAT A/A1086.cpp
+++ OJ/PAT/PAT A/A1086.cpp
@@ -1,66 +1,67 @@
+/*
+ * 用"根据二叉树特点建树"的方法重做该题发现是 PA, 21/25 测试点五越界, 说明是细节问题
+ *     所以不纠结了。
+ * 另外我才发现这道题正确思路应该是"根据前序和中序进行建树"。其中, 前序看 Push 序列,
+ *     中序看 Pop 序列。这个我确实做两次都没发现..
+ */
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stack>
 using namespace std;
 
-struct node_t {
+typedef struct node_t {
 	int data;
-	node_t *left;
-	node_t *right;
-};
+	struct node_t* left, * right;
+}node_t;
 
-static node_t *root;//root 保存中序序列遍历完后建立的树的根
-static stack<string> s;
+const int maxn = 31;
+int n, ptr = 0;//指示"栈顶"
+string fake_stack[maxn];//"栈"
+vector<int > post;
 
-node_t *create() {
-	if (s.empty() || s.top() == "Pop") {//栈空或遍历到 Pop 时返回NULL
-		if (!s.empty())    s.pop();
-		return NULL;
-	}
+/* 建树: 观察发现 Push 创建的必是左孩子, Pop 分情况讨论 */
+node_t* create() {
+	node_t* node = new node_t;
+	string top = fake_stack[ptr];//"取栈顶"
+	ptr++;//"出栈"
+	
+	if (top.size() > 3) {//"Push"
+		int data = atoi(top.erase(0, 5).c_str());//先去掉前 5 个字符, 然后转化为整型
+		node->data = data;
+		node->left = create();
+		if (ptr < n && fake_stack[ptr].size() > 5)    node->right = create();
+		else {
+			ptr++;
+			node->right = NULL;
+		}
+	} else    return NULL;
 
-	node_t *root = new node_t;
-	string str = s.top();
-	s.pop();
-	if (str.size() == 6)    root->data = str[5] - '0';
-	else    root->data = (str[5] - '0') * 10 + str[6] - '0';
-	root->left = create();
-	root->right = create();
-
-	return root;
+	return node;
 }
 
-void post_traversal(node_t *tree) {
-	if (tree == NULL)    return;
+/* 后序遍历 */
+void post_travesal(node_t* t) {
+	if (t == NULL)    return;
 
-	post_traversal(tree->left);
-	post_traversal(tree->right);
-	if (tree == root)    cout << tree->data;//后序最后一个即根结点
-	else    cout << tree->data << " ";
+	post_travesal(t->left);
+	post_travesal(t->right);
+	post.push_back(t->data);
 }
 
 int main() {
-	int n;
-
-	/* 1. 输入 */
+	/* 1. INPUT MODULE */
 	cin >> n;
-	cin.ignore();
-	vector<string> str;
-	string inp;
-	for (int i = 0; i < 2 * n; ++i) {//先将输入保存到某个地方
-		getline(cin, inp);
-		str.push_back(inp);
-	}
-	vector<string>::iterator it = str.end() - 1;
-	for (; it != str.begin(); --it)    s.push(*it);//入栈要反过来这样栈顶才是第一个
-	s.push(*it);//for() 遍历到第一个迭代器会退出所以最后还要补上
+	n *= 2;
+	cin.get();//吃掉上面"cin >> n"的回车
+	for (int i = 0; i < n; ++i)    getline(cin, fake_stack[i]);
 
-	/* 2. 主逻辑 */
-	node_t *tree = create();
-	root = tree;
+	/* 2. MAIN LOGIC */
+	node_t* tree = create();
+	post_travesal(tree);
 
-	/* 3. 输出 */
-	post_traversal(tree);
+	/* 3. OUTPUT MODULE */
+	cout << post[0];
+	for (int i = 1; i < (int)post.size(); ++i)    cout << " " << post[i];
 
 	return 0;
 }
