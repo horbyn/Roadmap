1d0cfff958b48fd5065ea24c822d9fbcc56b97db
hoRbyn <hor4bynZ@gmail.com>
2021-07-19 11:29:08 +0800

    A1062 AC(25), 35min

--- OJ/PAT/PAT A/A1062.cpp
+++ OJ/PAT/PAT A/A1062.cpp
@@ -1,61 +1,56 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
-#include <cstring>
-
-#define MAXSIZE 1024
-
+#pragma warning(disable: 4996)
 using namespace std;
 
-struct man_t {
-    char num[9];
-    int  vir;
-    int  tal;
-    int  gra;
-    int  typ;
-};
+typedef struct man_t {
+	int id, vir, tel, total, type;
+}man_t;
 
-bool cmp(struct man_t a, struct man_t b) {
-    if (a.typ != b.typ)    return a.typ < b.typ;
-    if (a.gra != b.gra)    return a.gra >= b.gra;
-    else if (a.vir != b.vir)    return a.vir >= b.vir;
-    else    return strcmp(a.num, b.num) < 0;
+const int maxn = 100000;
+int n, l, h;
+man_t man[maxn];
+
+/* 比较函数 */
+bool cmp(man_t a, man_t b) {
+	if (a.type != b.type)    return a.type > b.type;//按司马光规则排序
+	else if (a.total != b.total)    return a.total > b.total;//都是一类人则按总分排序
+	else if (a.vir != b.vir)    return a.vir > b.vir;//总分相同按美德分排序
+	else    return a.id < b.id;//美德分相同按 id 排序
 }
 
-int main()
-{
-    struct man_t man[MAXSIZE] = { 0 };
-    int n, l, h;
-    int tot = 0;
+/* 判断一个人类型 */
+int judge_man(int v, int t) {
+	if (t >= h && v >= h)    return 4;//圣人
+	else if (t < h && v >= h)    return 3;//君子
+	else if (t < h && v < h && v >= t)    return 2;//愚人
+	else    return 1;//小人
+}
 
-    cin >> n >> l >> h;
-    for (int i = 0; i < n; ++i) {
-        cin >> man[i].num >> man[i].vir >> man[i].tal;
-        //1. 最低要求
-        if (man[i].vir < l || man[i].tal < l) {
-            --i;
-            --n;
-            continue;
-        }
+int main() {
+	/* 1. INPUT MODULE */
+	scanf("%d%d%d", &n, &l, &h);
+	for (int i = 0; i < n; ++i) {
+		int inp_id, inp_vir, inp_tel;
+		scanf("%d%d%d", &inp_id, &inp_vir, &inp_tel);
+		if (inp_vir >= l && inp_tel >= l) {
+			man[i].id = inp_id;
+			man[i].vir = inp_vir;
+			man[i].tel = inp_tel;
+			man[i].total = inp_vir + inp_tel;
+			man[i].type = judge_man(inp_vir, inp_tel);
+		} else {//对于分数均低于门槛 L 的直接过滤掉
+			n--;
+			i--;
+		}
+	}
 
-        //2. 算总分
-        else    man[i].gra = man[i].vir + man[i].tal;
-        //3. 分类
-        if (man[i].vir >= h && man[i].tal >= h) //圣人
-            man[i].typ = 1;
-        else if (man[i].vir >= h && h > man[i].tal) //君子
-            man[i].typ = 2;
-        else if (h > man[i].vir && h > man[i].tal
-            && man[i].vir >= man[i].tal) //愚人
-            man[i].typ = 3;
-        else //小人
-            man[i].typ = 4;
-        tot++;
-    }
-    sort(man, man + n, cmp);
+	/* 2. MAIN LOGIC */
+	sort(man, man + n, cmp);
 
-    cout << tot << endl;
-    for (int i = 0; i < tot; ++i)
-        cout << man[i].num << " "
-            << man[i].vir << " "
-            << man[i].tal << endl;
+	/* 3. OUTPUT MODULE */
+	printf("%d\n", n);
+	for (int i = 0; i < n; ++i)    printf("%08d %d %d\n", man[i].id, man[i].vir, man[i].tel);
+
+	return 0;
 }
