22af1741e89148831c0e52fcc9c6abd02089c0fd
hoRbyn <hor4bynZ@gmail.com>
2021-08-19 09:32:32 +0800

    A1053, 47min, PA(27/30); A1079, 40min, ACC(25)

--- OJ/PAT/PAT A/A1053.cpp
+++ OJ/PAT/PAT A/A1053.cpp
@@ -1,71 +1,64 @@
+/* PA 27/30 */
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef struct node_t {//一般树组织方式: data + vector
-	int          data;
+typedef struct node_t {//一般树组织
+	int weight;
 	vector<int > child;
 }node_t;
 
 const int maxn = 100;
-int n, m, s;
-vector<int > path_tmp;
-vector<vector<int > > path;
+int n, m, s, ptr = 0;
 node_t tree[maxn];
+vector<int > path[maxn], tmp;
 
-bool cmp(vector<int > a, vector<int > b) {//二维数组比较大小的 cmp() 思路: 逐个遍历
-	int len = (int)a.size() <= (int)b.size() ? (int)a.size() : (int)b.size();
+/* 二维数组比较: 逐个遍历 */
+bool cmp(vector<int > a, vector<int > b) {
+	int len = a.size() <= b.size() ? a.size() : b.size();
 	for (int i = 0; i < len; ++i) {
 		if (a[i] == b[i])    continue;
 		else    return a[i] > b[i];
 	}
 }
 
-void dfs(node_t *node, int sum) {//遍历 root->leaves 用 dfs()
-	sum = sum + node->data;
-	path_tmp.push_back(node->data);
+void dfs(int v, int t) {
+	tmp.push_back(tree[t].weight);
+	v += tree[t].weight;
 
-	if (node->child.empty()) {
-		if (sum == s)    path.push_back(path_tmp);
-		path_tmp.pop_back();
-		return;
-	}
-	if (sum < s) {
-		for (int i = 0; i < (int)node->child.size(); ++i) {
-			int child = node->child[i];
-			dfs(&tree[child], sum);
-		}
-	}
-	path_tmp.pop_back();
+	//遍历每个孩子
+	for (int i = 0; i < (int)tree[t].child.size(); ++i)
+		dfs(v, tree[t].child[i]);
+	//判断是否满足条件
+	if (tree[t].child.empty() && v == s)
+		path[ptr++] = tmp;
+
+	tmp.pop_back();
 }
 
 int main() {
 	/* 1. INPUT MODULE */
 	cin >> n >> m >> s;
-	for (int i = 0; i < n; ++i) {
-		int inp;
-		cin >> inp;
-		tree[i].data = inp;
-	}
+	for (int i = 0; i < n; ++i)    cin >> tree[i].weight;
 	for (int i = 0; i < m; ++i) {
-		int node, num;
-		cin >> node >> num;
+		int idx, num, inp;
+		cin >> idx >> num;
 		for (int j = 0; j < num; ++j) {
-			int leaf;
-			cin >> leaf;
-			tree[node].child.push_back(leaf);
+			cin >> inp;
+			tree[idx].child.push_back(inp);
 		}
 	}
 
 	/* 2. MAIN LOGIC */
-	dfs(&tree[0], 0);
+	dfs(0, 0);
+	sort(path, path + ptr, cmp);
 
 	/* 3. OUTPUT MODULE */
-	sort(path.begin(), path.end(), cmp);
-	for (int i = 0; i < (int)path.size(); ++i) {
+	for (int i = 0; i < ptr; ++i) {
 		cout << path[i][0];
-		for (int j = 1; j < (int)path[i].size(); ++j)    cout << " " << path[i][j];
+		for (int j = 1; j < (int)path[i].size(); ++j)
+			cout << " " << path[i][j];
 		cout << endl;
 	}
 
--- OJ/PAT/PAT A/A1079.cpp
+++ OJ/PAT/PAT A/A1079.cpp
@@ -1,54 +1,50 @@
 #include <cstdio>
 #include <vector>
+#include <cmath>
 #pragma warning(disable: 4996)
 using namespace std;
 
-typedef struct node_t {//一般数组织
-	double       data;
+typedef struct node_t {
+	int product;
 	vector<int > child;
 }node_t;
 
 const int maxn = 100000;
 int n;
-double p, r, sales = 0.0;
+double p, r, tot = 0.0;
 node_t tree[maxn];
+double rate[maxn];
 
-void dfs(node_t* t, double u) {//遍历 root->leaves 用 dfs()
-	if (t->child.empty()) {
-		sales += u * t->data;
-		return;
-	}
-
-	u *= 1.0 + r;
-	for (int i = 0; i < (int)t->child.size(); ++i)
-		dfs(&tree[t->child[i]], u);
+void dfs(int r, int t) {
+	for (int i = 0; i < (int)tree[t].child.size(); ++i)
+		dfs(r + 1, tree[t].child[i]);
+	tot += tree[t].product * rate[r] * p;//商品数量 * 比例 * 根单价
 }
 
 int main() {
 	/* 1. INPUT MODULE */
 	scanf("%d%lf%lf", &n, &p, &r);
-	r /= 100.0;
+	r = r / 100.0 + 1.0;
 	for (int i = 0; i < n; ++i) {
-		int m;
-		scanf("%d", &m);
-		if (m != 0) {
-			int inp;
-			for (int j = 0; j < m; ++j) {
+		int flag;
+		scanf("%d", &flag);
+		if (flag > 0) {
+			for (int j = 0; j < flag; ++j) {
+				int inp;
 				scanf("%d", &inp);
 				tree[i].child.push_back(inp);
 			}
-		} else {
-			int inp;
-			scanf("%d", &inp);
-			tree[i].data = inp;
+		} else if (flag == 0) {
+			scanf("%d", &tree[i].product);
 		}
 	}
 
-	/* 2. MAIN LOGIC */
-	dfs(&tree[0], p);
+	/* 2. MAIN LOGIC*/
+	for (int i = 0; i < maxn; ++i)    rate[i] = pow(r, i);//初始化比例数组
+	dfs(0, 0);
 
 	/* 3. OUTPUT MODULE */
-	printf("%.1lf", sales);
+	printf("%.1lf", tot);
 
 	return 0;
 }
