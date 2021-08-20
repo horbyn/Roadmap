ee0d97296050bb6af40922bc2677c199505ffe48
hoRbyn <hor4bynZ@gmail.com>
2021-08-20 10:20:44 +0800

    A1086, 45min, AC(25)

--- OJ/PAT/PAT A/A1086.cpp
+++ OJ/PAT/PAT A/A1086.cpp
@@ -1,12 +1,7 @@
-/*
- * 用"根据二叉树特点建树"的方法重做该题发现是 PA, 21/25 测试点五越界, 说明是细节问题
- *     所以不纠结了。
- * 另外我才发现这道题正确思路应该是"根据前序和中序进行建树"。其中, 前序看 Push 序列,
- *     中序看 Pop 序列。这个我确实做两次都没发现..
- */
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 using namespace std;
 
 typedef struct node_t {
@@ -15,27 +10,23 @@ typedef struct node_t {
 }node_t;
 
 const int maxn = 31;
-int n, ptr = 0;//指示"栈顶"
-string fake_stack[maxn];//"栈"
-vector<int > post;
+int n;
+vector<int > pre, in, post;
+stack<int > sta;
 
-/* 建树: 观察发现 Push 创建的必是左孩子, Pop 分情况讨论 */
-node_t* create() {
+/* 建树: 根据前中序列建树 */
+node_t* create(int preB, int preE, int inB, int inE) {
+	if (preB > preE)    return NULL;
+
+	int root = pre[preB];
+	int k = 0;
+	for (; k <= inE && in[k] != root; ++k);
+	int left_len = k - inB;
+
 	node_t* node = new node_t;
-	string top = fake_stack[ptr];//"取栈顶"
-	ptr++;//"出栈"
-	
-	if (top.size() > 3) {//"Push"
-		int data = atoi(top.erase(0, 5).c_str());//先去掉前 5 个字符, 然后转化为整型
-		node->data = data;
-		node->left = create();
-		if (ptr < n && fake_stack[ptr].size() > 5)    node->right = create();
-		else {
-			ptr++;
-			node->right = NULL;
-		}
-	} else    return NULL;
-
+	node->data = root;
+	node->left = create(preB + 1, preB + left_len, inB, k - 1);
+	node->right = create(preB + left_len + 1, preE, k + 1, inE);
 	return node;
 }
 
@@ -51,12 +42,25 @@ void post_travesal(node_t* t) {
 int main() {
 	/* 1. INPUT MODULE */
 	cin >> n;
-	n *= 2;
-	cin.get();//吃掉上面"cin >> n"的回车
-	for (int i = 0; i < n; ++i)    getline(cin, fake_stack[i]);
+	cin.get();//前面有 cin, 要调用一次 .get() 吃掉回车
+	for (int i = 0; i < 2 * n; ++i) {
+		string str;
+		getline(cin, str);//因为输入有空格, 所以用 getline() 获取整行
+		if (str[1] == 'u') {
+			str.erase(str.begin(), str.begin() + 5);
+			int dig = atoi(str.c_str());
+			sta.push(dig);
+			pre.push_back(dig);//入栈序列为先序
+		}
+		else {
+			int dig = sta.top();
+			sta.pop();
+			in.push_back(dig);///出栈序列为中序
+		}
+	}
 
 	/* 2. MAIN LOGIC */
-	node_t* tree = create();
+	node_t* tree = create(0, n - 1, 0, n - 1);
 	post_travesal(tree);
 
 	/* 3. OUTPUT MODULE */
