3a56bf23411e3e999f9592dde1eab0675b332de1
hoRbyn <hor4bynZ@gmail.com>
2021-08-22 10:00:03 +0800

    A1051, 28min, AC(25)

--- OJ/PAT/PAT A/A1051.cpp
+++ OJ/PAT/PAT A/A1051.cpp
@@ -1,51 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <stack>
 using namespace std;
 
-static int m, n, k;//m 栈容量, n 序列长度, k 序列个数
+const int maxn = 1000;
+int m, n, k;
+int seq[maxn][maxn];
+stack<int > sta;
 
-bool is_pop_seq(vector<int>& seq_pus, vector<int>& seq_pop) {
-	stack<int> s;
-	int ptr_pus = 0;//ptr_pus 从固定的入栈序列中取数
-	int ptr_pop = 0;//ptu_pop 从出栈序列中取数
+/* 判断序列是否是出栈序列 */
+bool judge(int i) {
+	while (!sta.empty())    sta.pop();//初始化栈
 
-	while (s.size() <= (unsigned)m && ptr_pop < n) {
-		if (s.empty())    s.push(seq_pus[ptr_pus++]);
+	int v = 1, j = 0;//v 指示入栈数值, j 指示序列当前元素
+	sta.push(v++);
+	while (!sta.empty()) {
+		int top = sta.top();//获取栈顶数值
+		if (top == seq[i][j]) {//若栈顶与序列元素相同
+			sta.pop();
+			j++;
+			if (sta.empty()) {
+				if (v != n + 1)    sta.push(v++);
+				else    return true;
+			}
+		} else {//若栈顶与序列元素不同
+			if (sta.size() == m)    return false;
 
-		if (s.top() == seq_pop[ptr_pop]) {//栈顶元素等于出栈序列元素
-			s.pop();
-			ptr_pop++;
-		} else {
-			if (ptr_pus < n && s.size() < (unsigned)m) {
-				s.push(seq_pus[ptr_pus++]);//入栈序列还有元素，则入栈该元素
-			} else    return false;//栈顶元素不等于出栈序列元素，且入栈序列已全部入完
+			if (v != n + 1)    sta.push(v++);
+			else    return false;
 		}
 	}
-	if (s.size() == (unsigned)m)    return false;//退出是因为栈满则出栈序列错误
-	else    return true;//否则栈空即为符合的出栈序列
 }
 
 int main() {
+	/* 1. INPUT MODULE */
 	cin >> m >> n >> k;
+	for (int i = 0; i < k; ++i)
+		for (int j = 0; j < n; ++j)
+			cin >> seq[i][j];
 
-	vector<int> seq_pus;//入栈序列
-	vector<vector<int> > seq_pop;//出栈序列
-	int inp;
-	for (int i = 1; i <= n; ++i)    seq_pus.push_back(i);
-	for (int i = 0; i < k; ++i) {
-		vector<int> tmp;
-		for (int j = 0; j < n; ++j) {
-			cin >> inp;
-			tmp.push_back(inp);
-		}
-		seq_pop.push_back(tmp);
-	}
-
-	for (int i = 0; i < k; ++i) {
-		if (is_pop_seq(seq_pus, seq_pop[i]))    cout << "YES\n";
+	/* 2. MAIN LOGIC */
+	for (int i = 0; i < k; ++i)
+		/* 3. OUTPUT MODULE */
+		if (judge(i))    cout << "YES\n";
 		else    cout << "NO\n";
-	}
 
 	return 0;
 }
