41f1a2bbff6d8ebf8fb6f1387eb6cefe404d3d88
hoRbyn <hor4bynZ@gmail.com>
2021-08-23 09:41:49 +0800

    A1107, 48min, PA(17/30)

--- OJ/PAT/PAT A/A1107.cpp
+++ OJ/PAT/PAT A/A1107.cpp
@@ -3,22 +3,28 @@
 #include <algorithm>
 using namespace std;
 
-static const int maxn = 1000;
-static int hobbies[maxn] = { 0 }, father[maxn] = { 0 }, root[maxn] = { 0 };
+const int maxn = 1001;
+int n;
+int father[maxn], isRoot[maxn];
+int hobby[1001];
 
-static int find_father(int child) {
-	while (father[child] != child) {
-		child = father[child];
+int find_father(int x) {
+	int a = x;
+	while (x != father[x])    x = father[x];
+
+	while (a != father[a]) {
+		int fa = father[a];
+		father[a] = x;
+		a = fa;
 	}
-	return child;
+
+	return x;
 }
 
-static void union_set(int a, int b) {
-	int fa = find_father(a);
-	int fb = find_father(b);
-	if (fa != fb) {
-		father[fa] = fb;
-	}
+void merge(int a, int b) {
+	int x = find_father(a);
+	int y = find_father(b);
+	if (x != y)    father[y] = x;
 }
 
 bool cmp(int a, int b) {
@@ -26,36 +32,33 @@ bool cmp(int a, int b) {
 }
 
 int main() {
-	int n;
-	for (int i = 0; i < maxn; ++i)    father[i] = i;
+	/* 0. INITIALIZATION */
+	for (int i = 1; i < maxn; ++i)    father[i] = i;
 
-	/* 1. INPUT && EXECUTE */
+	/* 1. INPUT MODULE */
 	cin >> n;
 	for (int i = 1; i <= n; ++i) {
-		string cou;
-		cin >> cou;
-		cou.erase(cou.end() - 1);
-		int m = atoi(cou.c_str()), x;
-		for (int j = 0; j < m; ++j) {
-			cin >> x;
-			if (hobbies[x] == 0)    hobbies[x] = i;//爱好未属于其他人
-
-			union_set(i, find_father(hobbies[x]));//合并这个人和拥有这项爱好的人
+		string tmp;
+		int num;
+		cin >> num >> tmp;
+		for (int j = 0; j < num; ++j) {
+			int inp;
+			cin >> inp;
+			if (hobby[inp] == 0)    hobby[inp] = i;
+			
+			merge(find_father(hobby[inp]), i);
 		}
 	}
 
-	/* 2. OUTPUT */
-	for (int i = 1; i <= n; ++i) {
-		int r = find_father(i);
-		if (r == i)    root[i]++;
-		else    root[r]++;
-	}
-
-	sort(root + 1, root + n + 1, cmp);
+	/* 2. MAIN LOGIC */
+	for (int i = 1; i <= n; ++i)    isRoot[find_father(i)]++;
 	int num = 0;
-	for (int i = 1; i <= n && root[i] != 0; ++i, ++num);
-	cout << num << endl << root[1];
-	for (int i = 2; i <= num; ++i)    cout << " " << root[i];
+	for (; isRoot[num + 1] != 0; ++num);
+	sort(isRoot + 1, isRoot + num + 1, cmp);
+
+	/* 3. OUTPUT MODULE */
+	cout << num << endl << isRoot[1];
+	for (int i = 2; i <= num; ++i)    cout << " " << isRoot[i];
 
 	return 0;
 }
