b5835c159427ed91a7ab942ee94f5ec49e2db71b
hoRbyn <hor4bynZ@gmail.com>
2021-08-27 09:55:20 +0800

    A1016, 70min, PA(15/25); A1113, 13min, AC(25/25)

--- OJ/PAT/PAT A/A1016.cpp
+++ OJ/PAT/PAT A/A1016.cpp
@@ -1,3 +1,4 @@
+/* PA(15/25) */
 #include <cstdio>
 #include <iostream>
 #include <string>
@@ -7,85 +8,68 @@ using namespace std;
 
 typedef struct record_t {
 	string name;
-	int mon, d, h, m;
+	int MM, dd, HH, mm, dura;
 	bool flag;
-}record_t;
+}rec_t;
 
 const int maxn = 1000;
-int n, cost[24];
-record_t rec[maxn];
+int n;
+int rate[24];
+rec_t rec[maxn];
 
-/* 比较函数 */
-bool cmp(record_t a, record_t b) {
+bool cmp(rec_t a, rec_t b) {
 	if (a.name != b.name)    return a.name < b.name;
-	else if (a.mon != b.mon)    return a.mon < b.mon;
-	else if (a.d != b.d)    return a.d < b.d;
-	else if (a.h != b.h)    return a.h < b.h;
-	else    return a.m < b.m;
+	else    return a.dura < b.dura;
 }
 
 int main() {
 	/* 1. INPUT MODULE */
-	for (int i = 0; i < 24; ++i)    scanf("%d", &cost[i]);
+	for (int i = 0; i < 24; ++i)    scanf("%d", &rate[i]);
 	scanf("%d", &n);
 	for (int i = 0; i < n; ++i) {
-		string flag;
+		string tmp;
 		cin >> rec[i].name;
-		scanf("%d:%d:%d:%d", &rec[i].mon, &rec[i].d, &rec[i].h, &rec[i].m);
-		cin >> flag;
-		if (flag == "on-line")    rec[i].flag = true;
+		scanf("%d:%d:%d:%d", &rec[i].MM, &rec[i].dd, &rec[i].HH, &rec[i].mm);
+		cin >> tmp;
+		rec[i].dura = rec[i].dd * 1440 + rec[i].HH * 60 + rec[i].mm;//计算持续时间
+		if (tmp == "on-line")    rec[i].flag = true;
 		else    rec[i].flag = false;
 	}
 
 	/* 2. MAIN LOGIC */
 	sort(rec, rec + n, cmp);
-	double charge = 0.0;
-	string name = rec[0].name;
-	cout << name;
-	printf(" %02d\n", rec[0].mon);
+	double sum = 0.0;
+	string cur = "";
 	for (int i = 0; i < n; ++i) {
-		int min = 0;
-		double tmp_charge = 0.0;
-
-		if (rec[i].flag && i + 1 < n && !rec[i + 1].flag) {
-			if (name != rec[i].name) {
-				printf("Total amount: $%.2lf\n", charge / 100.0);
-				charge = 0.0;
-			}
-
-			int d = rec[i].d, h = rec[i].h, m = rec[i].m;
-
-			//处理时间的逻辑: 核心思路就是拆成一分钟一分钟地去算
-			for (; d <= rec[i + 1].d; ++d) {
-				int end_h = d < rec[i + 1].d ? 23 : rec[i + 1].h;
-				for (; h <= end_h; ++h) {
-					int end_m;
-					if (d == rec[i + 1].d)
-						end_m = h < rec[i + 1].h ? 60 : rec[i + 1].m;
-					else    end_m = 60;
-
-					for (; m < end_m; ++m) {
-						min++;
-						tmp_charge += (double)cost[h];
+		if (rec[i].flag) {
+			if (i + 1 < n && !rec[i + 1].flag) {//一条记录
+				if (cur != rec[i].name) {//不是同一个人, 那就输出上一个人的总价
+					if (i != 0) {
+						printf("Total amount: $%.2lf\n", sum / 100.0);
+						sum = 0.0;
 					}
-					m = 0;
+					cur = rec[i].name;
+					cout << rec[i].name;
+					printf(" %02d\n", rec[i].MM);
 				}
-				h = 0;
-			}
-		} else    continue;
-		charge += tmp_charge;
 
-		/* 3. OUTPUT MODULE */
-		if (name != rec[i].name) {
-			name = rec[i].name;
-			cout << name;
-			printf(" %02d\n", rec[i].mon);
+				double tmp = 0.0;
+				for (int t = rec[i].dura; t < rec[i + 1].dura; ++t) {//这对记录持续时间内的话费
+					tmp += rate[t % 1440 / 60];
+				}
+				sum += tmp;
+
+				/* 3. OUTPUT MODULE */
+				printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2lf\n",
+					rec[i].dd, rec[i].HH, rec[i].mm,
+					rec[i + 1].dd, rec[i + 1].HH, rec[i + 1].mm,
+					rec[i + 1].dura - rec[i].dura, tmp / 100.0);
+
+				++i;
+			}
 		}
-		printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2lf\n",
-			rec[i].d, rec[i].h, rec[i].m, rec[i + 1].d,
-			rec[i + 1].h, rec[i + 1].m, min, tmp_charge / 100.0);
 	}
-	printf("Total amount: $%.2lf\n", charge / 100.0);
+	printf("Total amount: $%.2lf\n", sum / 100.0);
 
 	return 0;
 }
--- OJ/PAT/PAT A/A1113.cpp
+++ OJ/PAT/PAT A/A1113.cpp
@@ -1,56 +1,30 @@
-/* 用随机选择算法反而超时？ */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
-#pragma warning(disable: 4996)
+#include <cmath>
 using namespace std;
 
-const int maxn = 100001;
-int n, a[maxn];
-
-/* 划分子序列: 使中间某个元素 pivot 总是大于等于左子列, 小于等于右子列 */
-int partition(int l, int r) {
-	int pivot = a[l];
-
-	int i = l, j = r;
-	while (i < j) {
-		for (; i < j && a[j] > pivot; --j);
-		for (; i < j && a[i] <= pivot; ++i);
-		if (i != j)    swap(a[i], a[j]);
-	}
-	if (a[i] != pivot)    swap(a[i], a[l]);
-
-	return i;
-}
-
-/* 随机选择算法 */
-void random_select(int l, int r, int k) {
-	if (l == r)    return;
-
-	int pivot = partition(l, r);
-	int m = pivot - l + 1;
-	if (m == k)    return;
-	else if (m > k)    random_select(l, pivot - 1, k);
-	else    random_select(pivot + 1, r, k - m);
-}
+const int maxn = 100000;
+int n;
+int a[maxn];
+long long sum, s1;
 
 int main() {
 	/* 1. INPUT MODULE */
-	scanf("%d", &n);
-	int sum = 0;
-	for (int i = 1; i <= n; ++i) {
-		scanf("%d", &a[i]);
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
 		sum += a[i];
 	}
 
 	/* 2. MAIN LOGIC */
-	random_select(1, n, n / 2);
-	int s1 = 0;
-	for (int i = 1; i <= n / 2; ++i)    s1 += a[i];
+	sort(a, a + n);
+	for (int i = 0; i < n / 2; ++i)    s1 += a[i];
 
 	/* 3. OUTPUT MODULE */
-	if (n % 2 == 0)    printf("0 ");
-	else    printf("1 ");
-	printf("%d", sum - s1 - s1);//sum - s1 = s2
+	if (n % 2 == 0)    cout << "0 ";
+	else    cout << "1 ";
+	int s2 = sum - s1;
+	cout << abs(s2 - s1);
 
 	return 0;
 }
