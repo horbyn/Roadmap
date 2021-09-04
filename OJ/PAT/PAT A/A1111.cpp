354b8e889c29c9eb72589f2e4dd027016c69758c
horbyn-desktop <hor4bynz@gmail.com>
2021-09-04 11:08:52 +0800

    A1111, 55min, AC(30)

--- OJ/PAT/PAT A/A1111.cpp
+++ OJ/PAT/PAT A/A1111.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
+#include <vector>
 using namespace std;
 
-const int maxn = 501, inf = 0x3fffffff;
-bool vis[maxn] = { false };
-int n, s, d, min_rate = inf, min_sta = inf;//顶点数、起点、源点、最快、沿途最小站点
-int g[maxn][maxn], rate[maxn][maxn], dis[maxn], dis_rate[maxn];
-vector<int > pre[maxn], path, tmp_path;
-vector<int > pre_rate[maxn], path_rate;
+const int maxn = 500, inf = 0x3fffffff;
+int n, m, s, d;
+int graph[maxn][maxn], dis[maxn];
+int graph_time[maxn][maxn], dis_fast[maxn];
+int min_time = inf, min_num = inf;
+bool vis[maxn];
+vector<int > pre_dis[maxn], path_dis, path_tmp;
+vector<int > pre_fast[maxn], path_fast;
 
-void dijkstra(int s) {
+void dijkstra_dis(int s) {
 	//1. 初始化
+	memset(vis, false, sizeof(vis));
 	fill(dis, dis + maxn, inf);
 	dis[s] = 0;
 
@@ -19,141 +22,146 @@ void dijkstra(int s) {
 		//2. 找最小
 		int min = inf, u = -1;
 		for (int j = 0; j < n; ++j) {
-			if (!vis[j] && dis[j] < min) {
+			if (!vis[j] && min > dis[j]) {
 				min = dis[j];
 				u = j;
 			}
 		}
-		if (u == -1)    return;//剩余点两两间不连通
+		if (u == -1)    return;//当前所有点都遍历完, 或当前所有点都不连通
 		vis[u] = true;
 
 		//3. 松弛
 		for (int v = 0; v < n; ++v) {
-			if (!vis[v] && g[u][v] != inf) {
-				if (dis[v] > dis[u] + g[u][v]) {
-					dis[v] = dis[u] + g[u][v];
-					pre[v].clear();
-					pre[v].push_back(u);
-				} else if (dis[v] == dis[u] + g[u][v]) {
-					pre[v].push_back(u);
-				}
+			if (!vis[v] && graph[u][v] != inf) {
+				if (dis[v] > dis[u] + graph[u][v]) {
+					dis[v] = dis[u] + graph[u][v];
+					pre_dis[v].clear();
+					pre_dis[v].push_back(u);
+				} else if (dis[v] == dis[u] + graph[u][v])
+					pre_dis[v].push_back(u);
 			}
 		}
 	}
 }
 
-void dijkstra_rate(int s) {
+void dijkstra_fast(int s) {
 	//1. 初始化
 	memset(vis, false, sizeof(vis));
-	fill(dis_rate, dis_rate + maxn, inf);
-	dis_rate[s] = 0;
+	fill(dis_fast, dis_fast + maxn, inf);
+	dis_fast[s] = 0;
 
 	for (int i = 0; i < n; ++i) {
 		//2. 找最小
 		int min = inf, u = -1;
 		for (int j = 0; j < n; ++j) {
-			if (!vis[j] && dis_rate[j] < min) {
-				min = dis_rate[j];
+			if (!vis[j] && min > dis_fast[j]) {
+				min = dis_fast[j];
 				u = j;
 			}
 		}
-		if (u == -1)    return;//剩余点两两间不连通
+		if (u == -1)    return;//当前所有点都遍历完, 或当前所有点都不连通
 		vis[u] = true;
 
 		//3. 松弛
 		for (int v = 0; v < n; ++v) {
-			if (!vis[v] && rate[u][v] != inf) {
-				if (dis_rate[v] > dis_rate[u] + rate[u][v]) {
-					dis_rate[v] = dis_rate[u] + rate[u][v];
-					pre_rate[v].clear();
-					pre_rate[v].push_back(u);
+			if (!vis[v] && graph_time[u][v] != inf) {
+				if (dis_fast[v] > dis_fast[u] + graph_time[u][v]) {
+					dis_fast[v] = dis_fast[u] + graph_time[u][v];
+					pre_fast[v].clear();
+					pre_fast[v].push_back(u);
 				}
-				else if (dis_rate[v] = dis_rate[u] + rate[u][v]) {
-					pre_rate[v].push_back(u);
-				}
+				else if (dis_fast[v] == dis_fast[u] + graph_time[u][v])
+					pre_fast[v].push_back(u);
 			}
 		}
 	}
 }
 
-void dfs(int v) {
-	if (v == s) {
-		tmp_path.push_back(v);
-		//1. 计算沿途站点边权
+void dfs_dis(int u) {
+	if (u == s) {
+		path_tmp.push_back(u);
+		//1. 遍历路径
 		int value = 0;
-		for (int i = (int)tmp_path.size() - 1; i > 0; --i) {
-			int u = tmp_path[i], v = tmp_path[i - 1];
-			value += rate[u][v];
+		for (int i = (int)path_tmp.size() - 1; i > 0; --i) {
+			int cur = path_tmp[i], nxt = path_tmp[i - 1];
+			value += graph_time[cur][nxt];
 		}
-		//2. 判断
-		if (value < min_rate) {
-			min_rate = value;
-			path = tmp_path;
+		//2. 择优: 找符合题意的最值
+		if (min_time > value) {
+			min_time = value;
+			path_dis = path_tmp;
 		}
-		tmp_path.pop_back();
+		path_tmp.pop_back();
 	} else {
-		tmp_path.push_back(v);
-		for (int i = 0; i < (int)pre[v].size(); ++i)
-			dfs(pre[v][i]);
-		tmp_path.pop_back();
+		path_tmp.push_back(u);
+		for (int i = 0; i < (int)pre_dis[u].size(); ++i)    dfs_dis(pre_dis[u][i]);
+		path_tmp.pop_back();
 	}
 }
 
-void dfs_rate(int v) {
-	if (v == s) {
-		tmp_path.push_back(v);
-		//1. 计算沿途站点边权
-		int value = 0;
-		value = (int)tmp_path.size() - 2;//沿途站点等于扣除起点和终点
-		//2. 判断
-		if (value < min_sta) {
-			min_sta = value;
-			path_rate = tmp_path;
+void dfs_fast(int u) {
+	if (u == s) {
+		path_tmp.push_back(u);
+		//1. 遍历路径
+		int value = (int)path_tmp.size() - 1;
+		//2. 择优: 找符合题意的最值
+		if (min_num > value) {
+			min_num = value;
+			path_fast = path_tmp;
 		}
-		tmp_path.pop_back();
-	}
-	else {
-		tmp_path.push_back(v);
-		for (int i = 0; i < (int)pre_rate[v].size(); ++i)
-			dfs_rate(pre_rate[v][i]);
-		tmp_path.pop_back();
+		path_tmp.pop_back();
+	} else {
+		path_tmp.push_back(u);
+		for (int i = 0; i < (int)pre_fast[u].size(); ++i)    dfs_fast(pre_fast[u][i]);
+		path_tmp.pop_back();
 	}
 }
 
 int main() {
-	//1. INPUT MODULE
-	int m;
+	/* 0. INITIALIZATION */
+	fill(graph[0], graph[0] + maxn * maxn, inf);
+	fill(graph_time[0], graph_time[0] + maxn * maxn, inf);
+
+	/* 1. INPUT MODULE */
 	cin >> n >> m;
-	fill(g[0], g[0] + maxn * maxn, inf);
-	fill(rate[0], rate[0] + maxn * maxn, inf);
 	for (int i = 0; i < m; ++i) {
-		int u, v, flag, w1, w2;
-		cin >> u >> v >> flag >> w1 >> w2;
-		g[u][v] = w1;
-		rate[u][v] = w2;
-		if (flag == 0) {
-			g[v][u] = w1;
-			rate[v][u] = w2;
+		int v1, v2, flag, dis, tim;
+		cin >> v1 >> v2 >> flag >> dis >> tim;
+		if (flag == 1) {
+			graph[v1][v2] = dis;
+			graph_time[v1][v2] = tim;
+		} else {
+			graph[v1][v2] = graph[v2][v1] = dis;
+			graph_time[v1][v2] = graph_time[v2][v1] = tim;
 		}
 	}
 	cin >> s >> d;
 
-	//2. MAIN LOGIC
-	dijkstra(s);
-	dfs(d);
-	tmp_path.clear();
-	dijkstra_rate(s);
-	dfs_rate(d);
+	/* 2. MAIN LOGIC */
+	dijkstra_dis(s);
+	dfs_dis(d);
+	path_tmp.clear();
+	dijkstra_fast(s);
+	dfs_fast(d);
 
-	//3. OUTPUT MODULE
-	if (path == path_rate) {
-		cout << "Distance = " << dis[d] << "; Time = " << dis_rate[d] << ": " << s;
-		for (int i = (int)path.size() - 2; i >= 0; --i)    cout << " -> " << path[i];
+	/* 3. OUTPUT MODULE */
+	if (path_dis == path_fast) {
+		cout << "Distance = " << dis[d] << "; Time = " << dis_fast[d] << ": ";
+		for (int i = (int)path_dis.size() - 1; i >= 0; --i) {
+			cout << path_dis[i];
+			if (i != 0)    cout << " -> ";
+		}
 	} else {
-		cout << "Distance = " << dis[d] << ": " << s;
-		for (int i = (int)path.size() - 2; i >= 0; --i)    cout << " -> " << path[i];
-		cout << "\nTime = " << dis_rate[d] << ": " << s;
-		for (int i = (int)path_rate.size() - 2; i >= 0; --i)    cout << " -> " << path_rate[i];
+		cout << "Distance = " << dis[d] << ": ";
+		for (int i = (int)path_dis.size() - 1; i >= 0; --i) {
+			cout << path_dis[i];
+			if (i != 0)    cout << " -> ";
+		}
+		cout << "\nTime = " << dis_fast[d] << ": ";
+		for (int i = (int)path_fast.size() - 1; i >= 0; --i) {
+			cout << path_fast[i];
+			if (i != 0)    cout << " -> ";
+		}
 	}
 
 	return 0;
