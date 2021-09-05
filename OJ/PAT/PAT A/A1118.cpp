2e8bcc9c258cb05369b5d4959166c24dbcb45cfd
horbyn-desktop <hor4bynz@gmail.com>
2021-09-05 11:38:34 +0800

    A1118, 30min, AC(25)

--- OJ/PAT/PAT A/A1118.cpp
+++ OJ/PAT/PAT A/A1118.cpp
@@ -1,28 +1,28 @@
-/* PA 19/25 */
+/* 两个集合的关联: 同一幅图的即为同一个集合 */
 #include <iostream>
-#include <vector>
 using namespace std;
 
-const int maxn = 10000;
-int n, q;
-int father[maxn];
-bool isRoot[maxn];
-vector<int > query[maxn];
+const int maxn = 10001;
+int n, k, q;
+int birds[maxn], father[maxn];
+bool vis[maxn];
+int query[maxn][2];
 
 int find_father(int x) {
-	int a = x;
+	int backup = x;
+
 	while (x != father[x])    x = father[x];
 
-	while (father[a] != a) {
-		int fa = father[a];
-		father[a] = x;
-		a = fa;
+	while (backup != x) {
+		int backup_father = father[backup];
+		father[backup] = x;
+		backup = backup_father;
 	}
 
 	return x;
 }
 
-void merge(int a, int b) {
+void unite(int a, int b) {
 	int x = find_father(a);
 	int y = find_father(b);
 	if (x != y)    father[y] = x;
@@ -33,40 +33,35 @@ int main() {
 	for (int i = 0; i < maxn; ++i)    father[i] = i;
 
 	/* 1. INPUT MODULE */
-	int birds_num = -1;
+	int bird_num = -1;
 	cin >> n;
-	for (int i = 0; i < n; ++i) {
-		int num, root;
-		cin >> num;
-		for (int j = 0; j < num; ++j) {
-			int inp;
-			cin >> inp;
-			if (birds_num < inp)    birds_num = inp;//编号最大的就是鸟的数量
-			if (j == 0)    root = inp;//第一个输入作为集合的根
-			else    merge(root, inp);//之后的输入都合并到根
+	for (int i = 1; i <= n; ++i) {
+		cin >> k;
+		for (int j = 0; j < k; ++j) {
+			int tmp;
+			cin >> tmp;
+			if (bird_num < tmp)    bird_num = tmp;
+
+			if (birds[tmp] == 0)    birds[tmp] = i;
+			unite(find_father(birds[tmp]), i);
 		}
 	}
 	cin >> q;
-	for (int i = 0; i < q; ++i) {
-		int a, b;
-		cin >> a >> b;
-		query[i].push_back(a);
-		query[i].push_back(b);
-	}
+	for (int i = 0; i < q; ++i)    cin >> query[i][0] >> query[i][1];
 
 	/* 2. MAIN LOGIC */
-	int trees_num = 0;
-	for (int i = 1; i <= birds_num; ++i)    isRoot[find_father(i)] = true;
-	for (int i = 1; i <= birds_num; ++i)
-		if (isRoot[i] == true)    trees_num++;
+	for (int i = 1; i <= n; ++i)    vis[find_father(i)] = true;
+	int set_num = 0;
+	for (int i = 1; i <= n; ++i)
+		if (vis[i] == true)    set_num++;
 
 	/* 3. OUTPUT MODULE */
-	cout << trees_num << " " << birds_num << endl;
+	cout << set_num << " " << bird_num << endl;
 	for (int i = 0; i < q; ++i) {
-		int fa = find_father(query[i][0]);
-		int fb = find_father(query[i][1]);
-		if (fa == fb)    cout << "Yes\n";
-		else    cout << "No\n";
+		int b1 = query[i][0], b2 = query[i][1];
+		int x = find_father(birds[b1]), y = find_father(birds[b2]);
+		if (x != y)    cout << "No\n";
+		else    cout << "Yes\n";
 	}
 
 	return 0;
