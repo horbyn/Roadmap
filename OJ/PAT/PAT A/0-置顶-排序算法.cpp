f50d90f7e19cec041130c013ff5e60b6e523bfb8
horbyn <hor4bynz@gmail.com>
2022-01-22 18:19:04 +0800

    revise algorithm of sorting

--- OJ/PAT/PAT A/0-置顶-排序算法.cpp
+++ OJ/PAT/PAT A/0-置顶-排序算法.cpp
@@ -1,135 +1,144 @@
-/* 选择排序->堆排序
- * 插入排序
- * 冒泡排序(交换排序)->快速排序
- * 归并排序
- */
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int n = 10;
-int a[] = { 44, 13, 76, 25, 49, 58, 92, 33, 67, 82 };///除堆排其他从 0 开始
-int tmp[10] = {};
-int heap[] = { 0, 44, 13, 76, 25, 49, 58, 92, 33, 67, 82 };///堆排从 1 开始
-
+// 交换
 void swap(int* a, int* b) {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-//选择
-void selection_sort() {
-	for (int i = 0; i < n - 1; ++i) {
-		int min = i;
-		for (int j = i + 1; j < n; ++j)
-			if (a[min] > a[j])    min = j;
-		swap(a[i], a[min]);
-	}
+// 打印数组
+void print(vector<int >& v) {
+    cout << v[0];
+    for (int i = 1; i < (int)v.size(); ++i)
+        cout << " " << v[i];
+    cout << endl;
 }
 
-
-//插入
-void insertion_sort() {
-	for (int i = 1; i < n; ++i) {
-		int tmp = a[i], j = i;
-		for (; j > 0 && a[j - 1] > tmp; --j)    a[j] = a[j - 1];
-		swap(a[j], tmp);
-	}
+// 选择
+void selection_sort(vector<int >& v) {
+    for (int i = 0; i < (int)v.size() - 1; ++i) {
+        int mini = i;
+        for (int j = i + 1; j < (int)v.size(); ++j)
+            if (v[mini] > v[j])    mini = j;
+        swap(v[mini], v[i]);
+    }
 }
 
-//冒泡(即交换)
-void bubble_sort() {
-	for (int i = 0; i < n - 1; ++i)
-		for (int j = n - 1; j > i; --j)
-			if (a[j - 1] > a[j])    swap(a[j - 1], a[j]);
+// 插入
+void insertion_sort(vector<int >& v) {
+    for (int i = 1; i < (int)v.size(); ++i) {
+        int tmp = v[i], j = i;
+        for (; j > 0 && v[j - 1] > tmp; --j)
+            v[j] = v[j - 1];
+        v[j] = tmp;
+    }
 }
 
-//堆排(即选择)附件一
-void adjust_down(int l, int h) {
-	int i = l, j = 2 * i;
-	while (j <= h) {
-		if (j + 1 <= h && heap[j + 1] > heap[j])    j++;
-
-		if (heap[i] < heap[j]) {
-			swap(heap[i], heap[j]);
-			i = j;
-		} else    i = 2 * i;
-
-		j = 2 * i;
-	}
+// 冒泡(即交换)
+void bubble_sort(vector<int >& v) {
+    for (int i = 0; i < (int)v.size() - 1; ++i)
+        for (int j = (int)v.size() - 1; j > i; --j)
+            if (v[j - 1] > v[j])    swap(v[j - 1], v[j]);
 }
 
-//堆排(即选择)
-void heap_sort() {
-	//建堆
-	for (int i = n / 2; i >= 1; --i)    adjust_down(i, n);
+// 堆排(即选择)
+void adjust_down(vector<int >& v, int l, int h) {
+    int i = l, j = 2 * i;
+    while (j <= h) {
+        if (j + 1 <= h && v[j + 1] > v[j])    ++j;
 
-	//排序
-	for (int i = n; i > 1; --i) {
-		swap(heap[1], heap[i]);
-		adjust_down(1, i - 1);
-	}
+        if (v[i] < v[j]) {
+            swap(v[i], v[j]);
+            i = j;
+        }
+        else    i = 2 * i;
+        j = 2 * i;
+    }
 }
 
-//快排
-void quick_sort(int l, int r) {
-	if (l > r)    return;//递归必须要有循环条件
+void heap_sort(vector<int >& v) {
+    v.insert(v.begin(), 0);// 堆排从下标 1 开始
 
-	int pivot = l, i = l, j = r;
-	while (i != j) {
-		while (a[pivot] <= a[j] && i != j)    --j;
-		while (a[pivot] >= a[i] && i != j)    ++i;
-		if (i != j)    swap(a[i], a[j]);
-	}
-	swap(a[pivot], a[i]);
+    // 建堆
+    int size = (int)v.size() - 1;
+    for (int i = size / 2; i >= 1; --i)
+        adjust_down(v, i, size);
 
-	quick_sort(l, i - 1);
-	quick_sort(i + 1, r);
+    // 堆排
+    for (int i = size; i > 1; --i) {
+        swap(v[1], v[i]);
+        adjust_down(v, 1, i - 1);
+    }
+
+    v.erase(v.begin());// 删掉增加的下标 1 对应元素
 }
 
-//归并附件一
-void merge(int l, int r, int rE) {
-	int lE = r - 1, num = rE - l + 1;
-	int p = l;
+// 快排
+void quick_sort(vector<int >& v, int l, int r) {
+    if (l > r)    return;
 
-	while (l <= lE && r <= rE) {
-		if (a[l] < a[r])    tmp[p++] = a[l++];
-		else    tmp[p++] = a[r++];
-	}
-	while (l <= lE)    tmp[p++] = a[l++];
-	while (r <= rE)    tmp[p++] = a[r++];
+    int pivot = l, i = l, j = r;
+    while (i != j) {
+        while (i != j && v[j] >= v[pivot])    --j;
+        while (i != j && v[i] <= v[pivot])    ++i;
+        if (i != j)    swap(v[i], v[j]);
+    }
+    if (v[pivot] != v[i])    swap(v[pivot], v[i]);
 
-	for (int i = 0; i < num; ++i, --rE)    a[rE] = tmp[rE];
+    quick_sort(v, l, i - 1);
+    quick_sort(v, i + 1, r);
 }
 
-//归并附件二
-void submerge(int s) {
-	int i = 0;
-	for (; i <= n - 2 * s; i += 2 * s)    merge(i, i + s, i + 2 * s - 1);
-	if (i + s < n)    merge(i, i + s, n - 1);//最后两个子列
+//归并
+void merge(vector<int > &v, vector<int > &t, int l, int r, int rE) {
+    int lE = r - 1, num = rE - l + 1;
+    int p = l;
+
+    while (l <= lE && r <= rE) {
+        if (v[l] < v[r])    t[p++] = v[l++];
+        else    t[p++] = v[r++];
+    }
+    while (l <= lE)    t[p++] = v[l++];
+    while (r <= rE)    t[p++] = v[r++];
+
+    for (int i = 0; i < num; ++i, --rE)    v[rE] = t[rE];
 }
 
-//归并排序
-void merge_sort() {
-	int step = 1;//步长
-	while (step < n) {
-		submerge(step);
-		step *= 2;
-	}
+void submerge(vector<int >& v, vector<int >& t, int s) {
+    int i = 0, n = (int)v.size();
+    for (; i <= n - 2 * s; i += 2 * s)    merge(v, t, i, i + s, i + 2 * s - 1);
+    if (i + s < n)    merge(v, t, i, i + s, n - 1);//最后两个子列
+}
+
+void merge_sort(vector<int >& v, vector<int >& t) {
+    int step = 1, n = (int)v.size();//步长
+    while (step < n) {
+        submerge(v, t, step);
+        step *= 2;
+    }
 }
 
 int main() {
-	//EXECUATE
-	selection_sort();
-	//insertion_sort();
-	//bubble_sort();
-	//heap_sort();
-	//merge_sort();
-	//quick_sort(0, n - 1);
+    vector<int > arr;
+    int inp = 0;
 
-	//OUTPUT
-	for (int i = 0; i < n; ++i)    cout << a[i] << " ";///除堆排外其他函数的输出
-	//for (int i = 1; i <= n; ++i)    cout << heap[i] << " ";///堆排专用输出
+    // 输入 44 13 76 25 44 58 92 33 67 82
+    while (cin >> inp) {
+        arr.push_back(inp);
+        if (cin.get() == '\n')    break;
+    }
 
-	return 0;
+    //selection_sort(arr);
+    //insertion_sort(arr);
+    //bubble_sort(arr);
+    //heap_sort(arr);
+    //quick_sort(arr, 0, (int)arr.size() - 1);
+    vector<int > backup(arr);
+    merge_sort(arr, backup);
+    print(arr);
+
+    return 0;
 }
